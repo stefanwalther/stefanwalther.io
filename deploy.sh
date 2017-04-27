7279248314e6a4f69988d7a09848875c6abfa160
Stefan Walther <stefanwalther@users.noreply.github.com>
2017-04-28 00:34:33 +0200

    chore: deploy script

---
+++ deploy.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+
+DIR=$(dirname "$0")
+
+if [[ $(git status -s) ]]
+then
+    echo "The working directory is dirty. Please commit any pending changes."
+    exit 1;
+fi
+
+#echo "Deleting old publication"
+#rm -rf src/public
+#mkdir src/public
+#git worktree prune
+#rm -rf .git/worktrees/src/public/
+#
+#echo "Checking out gh-pages branch into public"
+#git worktree add -B gh-pages src/public upstream/gh-pages
+#
+#echo "Removing existing files"
+#rm -rf src/public/*
+#
+#echo "Generating site"
+#cd ./src
+#hugo
+#cd ..
+#
+#echo "Updating gh-pages branch"
+#cd src/public && git add --all && git commit -m "Publishing to gh-pages (publish.sh)"
